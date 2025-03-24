a82ba99d535c97007ecb14de2fefdf11de742d3d
ledrip418 <ledrip3@gmail.com>
2025-03-23 19:54:01 -0700

    Add files via upload

---
+++ CalculadoraIMC.py
@@ -0,0 +1,19 @@
+while True:
+    Peso = input("Ingresa tu peso: ")
+    Altura = input("ingresa tu altura en centimetros: ")
+    try:
+        float(Altura)
+        float(Peso)
+        if float(Altura) > 2.50:
+            print("La altura que ingresaste no es valida...Error 1")
+        elif float(Peso) > 700:
+            print("El peso que ingresaste no es valido...Error 1")
+        else:
+            Altura2 = float(Altura) * float(Altura)
+            Resultado = float(Peso) / float(Altura2)
+            print(Resultado)
+            input("Presiona ENTER para cerrar")
+            break
+    except ValueError:
+        print("Debes de ingresar solamente numeros...Error 0")
+
